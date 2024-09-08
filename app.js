10bfea951e7da485c012efad753e60aa4c9db009
Premnishok-Muhilan <premnishok4@gmail.com>
2024-09-09 00:14:14 +0530

    added the frontend deployed url in cors origin

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://delicate-salmiakki-625e68.netlify.app",
+    // origin: "https://delicate-salmiakki-625e68.netlify.app",
+    origin: "https://fantastic-elf-a6e68f.netlify.app"
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   })
